67658ae8f9748a10e4155cd0ab45cad8873441a4
eyalzek <eyal.ez@gmail.com>
2014-03-27 00:34:34 +0200

    changes

--- app/js/background.js
+++ app/js/background.js
@@ -1,26 +1,37 @@
 // gets code from popup and injects to active tab
 function injectTheScript(value) {
 	console.log(value);
-	chrome.tabs.executeScript(null, {code: value.code}, function() {
+	chrome.tabs.executeScript(null, value, function() {
 		console.log('Successfully injected script into the page ');
 	});
 }
 
 function saveToStorage(value) {
-	StorageArea.get(value.id, function(items) {
-		if (!(value.code in items)) {
-			StorageArea.set(value, function() {
+	chrome.storage.local.get(null, function(items) { // get local storage obj
+		var vals = _values(items); // get the values out
+		if (_.indexOf(vals, value.code) === -1) { // check if entered code doesn't exist in storage
+			chrome.storage.local.set(value, function() { // set it if not there
 				if (chrome.runtime.lastError) {
 					console.log(chrome.runtime.lastError);
 				}
+				else {
+					console.log("code: " + value.code + " is now in local storage");
+				}
 			});
+		}
+		else {
+			console.log("this code exists in local storage");
 		}
 	});
 }
 
 function initSavedCode() {
-	StorageArea.get("code", function(items) {
-		
-	})
-}
+	chrome.storage.local.get(null, function(items) { // get local storage obj
+		var that = $(".inner");
+		$.each(items, function(index, val) {
+			that.append("<p>" + val + "</p>"); // append each of the saved strings to the .inner element
+			console.log('appended');
+		}
+	);
+})}
 //chrome.tabs.onUpdated.addListener(injectTheScript);
--- app/js/popup.js
+++ app/js/popup.js
@@ -1,13 +1,35 @@
 var bg = chrome.extension.getBackgroundPage(),
 	link;
 
-document.addEventListener('DOMContentLoaded', function () {
-	chrome.tabs.query({windowType: "popup"}, function(Tab) {link = Tab[0].url});
-//	$('.container').innerHTML = "running script on: " + link;
-	$('button').on('click', function() {
-		alert('sending message');
-		if (bg && (typeof bg.injectTheScript === 'function')) {
-			bg.injectTheScript({code: $('.mycode').val(), id: "code"});
+$().ready(function() {
+	chrome.tabs.query({active: true, currentWindow: true, windowType: "normal"}, function(Tab) {link = Tab[0].url});
+//	$(".container").innerHTML = "running script on: " + link;
+	$(".submit").on("click", function() {
+		alert("sending message");
+		var that = {code: $(".mycode").val()};
+		//var that = $(".mycode").val();
+		if (bg && (typeof bg.injectTheScript === "function")) {
+			bg.injectTheScript(that);
+
+		}
+		if (bg && (typeof bg.saveToStorage === "function")) {
+			bg.saveToStorage(that);
 		}
 	});
+
+	if (bg && (typeof bg.initSavedCode === "function")) {
+		bg.initSavedCode();
+		console.log("history loaded");
+	}
+
+	$(".history").on('click', function() {
+		$(".inner").show();
+		console.log("clicked history button");
+	});
+	$("body").on("click", function(e) {
+		$(".inner").hide();
+		e.stopPropagation();
+	})
 });
+
+//document.addEventListener("DOMContentLoaded", 
